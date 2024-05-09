0c6bb9f322483ef889afa58980d41cd0ff5a6a5b
PaulSerg07 <paul_serg07@yahoo.com.br>
2024-05-09 09:45:55 -0300

    Mudança na exibição do texto
    Mudança no texto da página de login

--- template-materialui/src/App.js
+++ template-materialui/src/App.js
@@ -1,11 +1,10 @@
-import Header from "./components/header";
-import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import React from 'react';
+import Login from './pages/Login'; // Certifique-se de que o caminho para o arquivo Login está correto
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ListarTarefa />
+      <Login />
     </div>
   );
 }
